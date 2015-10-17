bf2d02ef348e95f86e3ffda39e225d678a8030a3
Jonathan Kochems <jonathankochems@users.noreply.github.com>
2015-10-18 00:46:17 +0100

    generalising repo type

--- src/Download/Internal.hs
+++ src/Download/Internal.hs
@@ -6,14 +6,28 @@ downloadToFile url path =
      content <- fetchURL url
      print content
      writeFile path content
-     
-gitDownload repo mods = 
+   
+data Repository = Repository{ repository :: String,
+                              prefix     :: String,
+                              branch     :: String
+                            }
+  
+gitDownload r targetdir mods = 
   do createDirectoryIfMissing True localDirPath
      downloadToFile url localPath
-  where url  = "https://raw.githubusercontent.com/"++repo++"/master/src/" ++ modPath
+  where url  = "https://raw.githubusercontent.com/"++repo++"/"++ branchName ++"/" ++ modPrefix ++ modPath
         modPath = dirPath ++ last mods ++ ".hs"
         dirPath = concat $ map (\x->x++"/") (init mods)
-        localDirPath = "test/" ++ dirPath
-        localPath    = "test/" ++ modPath
+        localDirPath = targetdir ++ dirPath
+        localPath    = targetdir ++ modPath
+        repo         = repository r
+        modPrefix    = prefix r
+        branchName   = branch r 
         
-bootstrap = gitDownload "jonathankochems/raskell-git-download" ["Download", "Internal"]
+bootstrap = gitDownload 
+              Repository{ repository="jonathankochems/raskell-git-download",
+                          prefix="src/",
+                          branch="master"
+                        }
+                        "test/"
+                        ["Download", "Internal"]
