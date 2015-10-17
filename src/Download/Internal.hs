b1b950275cff40eff4813b4472734fc68037d9d7
Jonathan Kochems <jonathankochems@users.noreply.github.com>
2015-10-18 00:31:44 +0100

    updating GitHub paths

--- src/Download/Internal.hs
+++ src/Download/Internal.hs
@@ -1,17 +1,19 @@
 import System.Directory
 import Raskell.Utils
---import Text.Parsec
-
 
 downloadToFile url path = 
-  do content <- fetchURL url
+  do print url
+     content <- fetchURL url
+     print content
      writeFile path content
      
-gitDownload mods = 
-  do createDirectoryIfMissing True dirPath
-     downloadToFile url modPath
-  where url  = "https://github.com/"++repo++"/raw/master/" ++ modPath
-        repo = "aslatter/parsec"
-        --modPath = "Text/Parsec.hs"
+gitDownload repo mods = 
+  do createDirectoryIfMissing True localDirPath
+     downloadToFile url localPath
+  where url  = "https://raw.githubusercontent.com/"++repo++"/master/src/" ++ modPath
         modPath = dirPath ++ last mods ++ ".hs"
         dirPath = concat $ map (\x->x++"/") (init mods)
+        localDirPath = "test/" ++ dirPath
+        localPath    = "test/" ++ modPath
+        
+bootstrap = gitDownload "jonathankochems/raskell-git-download" ["Download", "Internal"]
