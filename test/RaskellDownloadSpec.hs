dab7b5cfef9e5913bae210f70228405cbe017889
Jonathan Kochems <jonathankochems@users.noreply.github.com>
2015-10-20 20:18:30 +0100

    small test

--- test/RaskellDownloadSpec.hs
+++ test/RaskellDownloadSpec.hs
@@ -12,3 +12,7 @@ spec = --do
     it "should work fine" $
       -- TODO: write proper tests 
       rootDir raskellGitDownload `shouldBe` "../"
+      doesFileExist ".test_output_file" `shouldBe` False
+      downloadToFile "http://www.bogus.url/" ".test_output_file"
+      readFile ".test_output_file" `shouldBe` ""
+      deleteFile ".test_output_file"
