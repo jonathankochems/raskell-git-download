1069d4ab8f3f978b7f30e8bcd4e01555bee5db60
Jonathan Kochems <jonathankochems@users.noreply.github.com>
2015-10-21 09:43:18 +0100

    replacing dummy fetchURL

--- src/Raskell/Utils.hs
+++ src/Raskell/Utils.hs
@@ -1,4 +1,12 @@
 {-# LANGUAGE BangPatterns #-}
 module Raskell.Utils where
 
-fetchURL !url = return ""
+import qualified Network.HTTP    as HTTP 
+import qualified Network.Browser as Browser
+
+fetchURL !url = do 
+    (_, rsp) <- Browser.browse $ do
+                  Browser.setOutHandler (const $ return ())
+                  Browser.setAllowRedirects True -- handle HTTP redirects
+                  Browser.request $ HTTP.getRequest url
+    return $ HTTP.rspBody rsp
