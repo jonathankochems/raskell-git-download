791b3583940f2243d125276fbe331b71b75e66d0
Jonathan Kochems <jonathan.kochems@gmail.com>
2015-10-21 22:48:03 +0100

    wget

--- src/Raskell/Utils.hs
+++ src/Raskell/Utils.hs
@@ -1,12 +1,6 @@
 {-# LANGUAGE BangPatterns #-}
 module Raskell.Utils where
 
-import qualified Network.HTTP    as HTTP 
-import qualified Network.Browser as Browser
+import Network.HTTP.Wget (wget)
 
-fetchURL !url = do 
-    (_, rsp) <- Browser.browse $ do
-                  Browser.setOutHandler (const $ return ())
-                  Browser.setAllowRedirects True -- handle HTTP redirects
-                  Browser.request $ HTTP.getRequest url
-    return $ HTTP.rspBody rsp
+fetchURL !url = wget url [] []
