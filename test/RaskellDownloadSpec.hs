ad7eb9ecfe51d46b5318a0a0ca1eb40068facee4
Jonathan Kochems <jonathankochems@users.noreply.github.com>
2015-10-28 19:39:20 +0000

    adding test for downloadPackage. closes #2

--- test/RaskellDownloadSpec.hs
+++ test/RaskellDownloadSpec.hs
@@ -13,17 +13,30 @@ spec = do helpers
           gitdownload 
 
 gitdownload :: Spec
-gitdownload = 
-  describe "RaskellGitDownload" $ 
-    it "should download the requested Haskell modules from a GitHub repo" $ do
-      let path = ".tmp/Raskell/Test.hs"
-      exists <- doesFileExist ".tmp/Raskell/Test.hs" 
-      exists `shouldBe` False
-      let raskellGitDownloadRepo = Repository{ repository="jonathankochems/raskell-git-download",
+gitdownload = do
+  let path = ".tmp/Raskell/Test.hs"
+      raskellGitDownloadRepo = Repository{ repository="jonathankochems/raskell-git-download",
                                   			       prefix="src/",
                                       		     branch="develop"
                       						 }
+  describe "RaskellGitDownload" $ 
+    it "should download the requested Haskell modules from a GitHub repo" $ do
+      exists <- doesFileExist path
+      exists `shouldBe` False
       gitDownload raskellGitDownloadRepo ".tmp/" ["Raskell","Test"]
+      content <- readFile path 
+      content `shouldBe` "module Raskell.Test where\n"
+      removeFile path
+  describe "RaskellGitDownload" $ 
+    it "should download the requested Package from a GitHub repo" $ do
+      let package = Package{
+                      packageRepository = raskellGitDownloadRepo,
+                      rootDir = ".tmp/",
+                      modules = [["Raskell", "Test"]]
+                    }
+      exists <- doesFileExist path
+      exists `shouldBe` False
+      downloadPackage package
       content <- readFile path 
       content `shouldBe` "module Raskell.Test where\n"
       removeFile path
